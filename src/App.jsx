b177fe332a75c560e18372d22c1b4c65d5a22a3b
rabiulhossen <rhsimon69@gmail.com>
2026-09-12 17:48:22 +0600

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <h1>hello rubik</h1>
+        
+    </section>
+    </>
+  )
+}
+
+export default App
